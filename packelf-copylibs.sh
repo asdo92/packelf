bf1e23359ad7c650226bdeba445ed1f096487f13
q3aql <q3aql@duck.com>
2024-07-07 17:46:39 +0200

    Help message

--- packelf-copylibs.sh
+++ packelf-copylibs.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env sh
+#set -o pipefail # bash extension
+set -e
 
 if [ -z "${1}" ] ; then
-  echo "$0 <path-elf> <path-to-copy-libraries>"
+  echo "$0 <ELF_SRC_PATH> <PATH_TO_COPY_LIBRARIES>"
   exit 0
 else
   if [ -z "${2}" ] ; then
-    echo "$0 <path-elf> <path-to-copy-libraries>"
+    echo "$0 <ELF_SRC_PATH> <PATH_TO_COPY_LIBRARIES>"
     exit 0
   else
     libs="$(ldd "${1}" | grep -F '/' | sed -E 's|[^/]*/([^ ]+).*?|/\1|')"
